39cc308124368206e24a25ae40df31e8c69a9bd5
ppride25 <pingkoyletics@gmail.com>
2023-04-28 03:14:31 +0800

    initialized

---
+++ Part1/3_Row, Row, Row Your Boat.py
@@ -0,0 +1,16 @@
+# Write your solution here
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
+
+#added some .py extension
+#test add for 2nd pull request
+
+#adding add for 3rd pull
+#try adding this one, adding pull request for the badge test
+#commit also this one
+#or a 4rth one
+#this is a 5th commit
+#6th time
+#7th time
